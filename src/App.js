54362a75c6e8148da2150052274945a4dfb3acc1
veronicacastellanos <73354913+veronicacastellanos@users.noreply.github.com>
2020-12-13 17:58:40 +0100

    added some style

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ function App() {
   return (
     <div className="App">
     <div className="container">
-      <h1>Weather App</h1>
+      <h1>All Time Weather</h1>
       <Weather />
     </div>
         <footer>
-        <a className="source"
-          href="https://github.com/veronicacastellanos"
-          target="_blank"
-          rel="noopener noreferrer" ></a>
+          <a className="source"
+            href="https://github.com/veronicacastellanos/weather-react-project"
+            target="_blank"
+          >
+            Open-sourced on GitHub
+          </a>
         </footer>
     </div>
   );
