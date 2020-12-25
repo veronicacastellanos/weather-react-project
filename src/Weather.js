8232a26491acc332b0de0793a5241bfb81142370
veronicacastellanos <73354913+veronicacastellanos@users.noreply.github.com>
2020-12-25 22:10:37 +0100

    Added Weather Forecast and changed style

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast"
 
 export default function Weather(props) {
 const [weatherData, setWeatherData] = useState({ ready: false });
@@ -50,6 +51,7 @@ onChange={handleCityChange}
 />
 </form>
 <WeatherInfo data={weatherData}/>
+<WeatherForecast city={weatherData.city}/>
 </div>
 );
 } else {
---
+++ src/WeatherForecast.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./WeatherForecast.css"
+import WeatherForecastPreview from "./WeatherForecastPreview";
+
+export default function WeatherForecast(props) {
+const [loaded, setLoaded] = useState(false);
+const [forecast, setForecast] = useState(null);
+
+function handleForecastResponse(response) {
+setForecast(response.data);
+setLoaded(true);
+}
+
+if (loaded && props.city === forecast.city.name) {
+return (
+<div className="WeatherForecast row">
+<WeatherForecastPreview data={forecast.list[0]}/>
+<WeatherForecastPreview data={forecast.list[1]}/>
+<WeatherForecastPreview data={forecast.list[2]}/>
+<WeatherForecastPreview data={forecast.list[3]}/>
+<WeatherForecastPreview data={forecast.list[4]}/>
+<WeatherForecastPreview data={forecast.list[5]}/>
+</div>
+);
+} else {
+let apiKey = "28e5d779cb15e67167736268bc70d36f"
+let url = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+axios.get(url).then(handleForecastResponse);
+return null;
+}
+}
